15377f7883dc15a5c42cb385bffb9d887068eeaf
jojunhyung <cka830611@naver.com>
2025-03-31 10:23:24 +0900

    💄Feat : 리워드 광고 및 인앱 결제 기능 구현
    - 리워드 광고 시청 시 300코인 지급 (현재는 무제한으로 광고 시청 가능, 테스트 끝나고 갯수 제한 필요)
    - 구매 버튼 클릭 시:
      - 가격에 따라 코인 지급 메시지 출력
      - 구매 시 결제 완료 메시지 출력 후 2초 뒤 자동 창 닫힘
    - 코인 갯수 증가 코드 구현 완료 (현재는 주석처리됨)
    - GameManager에서 coin 데이터 관리 필요

--- Assets/02 Scripts/Shop/ShopButtonController.cs
+++ Assets/02 Scripts/Shop/ShopButtonController.cs
@@ -10,24 +10,24 @@ public class ShopButtonController : MonoBehaviour
 // 상점 버튼 클릭 시
 public void OnClickOpenShop()
     {
-        shopPanel.SetActive(true);  // 상점 패널을 활성화
+        shopPanel.SetActive(true); // 상점 패널을 활성화
     }
 
     // 상점 닫기 버튼 클릭 시
     public void OnClickCloseShop()
     {
         shopPanel.SetActive(false);  // 상점 패널을 비활성화
+       
     }
 
     public void shopPanelEsc()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)&& shopPanel.activeSelf)
         {
             if (shopPanel.activeSelf)
                 shopPanel.SetActive(false);
         }
     }
-
     private void Update()
     {
         shopPanelEsc();
