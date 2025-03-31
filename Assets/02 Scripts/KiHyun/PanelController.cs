3598bebf1b718b8776bed753ae568fb1319c355b
jojunhyung <cka830611@naver.com>
2025-03-31 13:36:14 +0900

    Remove unused comments
    주석제거

--- Assets/02 Scripts/KiHyun/PanelController.cs
+++ Assets/02 Scripts/KiHyun/PanelController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using DG.Tweening;
+using DG.Tweening;
 
 [RequireComponent(typeof(CanvasGroup))]
 public class PanelController : MonoBehaviour
@@ -26,8 +26,8 @@ public class PanelController : MonoBehaviour
         _backgroundCanvasGroup.alpha = 0;
         panelRectTransform.localScale = Vector3.zero;
 
-        //_backgroundCanvasGroup.DOFade(1, 0.3f).SetEase(Ease.Linear);
-        //panelRectTransform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
+        _backgroundCanvasGroup.DOFade(1, 0.3f).SetEase(Ease.Linear);
+        panelRectTransform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
     }
 
     /// <summary>
@@ -38,12 +38,12 @@ public class PanelController : MonoBehaviour
         _backgroundCanvasGroup.alpha = 1;
         panelRectTransform.localScale = Vector3.one;
 
-        //_backgroundCanvasGroup.DOFade(0, 0.3f).SetEase(Ease.Linear);
-        //panelRectTransform.DOScale(0, 0.3f)
-        /*    .SetEase(Ease.InBack).OnComplete(() =>
+        _backgroundCanvasGroup.DOFade(0, 0.3f).SetEase(Ease.Linear);
+        panelRectTransform.DOScale(0, 0.3f)
+            .SetEase(Ease.InBack).OnComplete(() =>
             {
                 hideDelegate?.Invoke();
                 Destroy(gameObject);
             });
-    */}
+    }
 }
